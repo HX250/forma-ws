fd5dc28f83c92190669e5f6e5ca97799be04c0be
Rastislav Pacut <rastislavpacut250@gmail.com>
2025-08-03 13:00:22 +0200

    testing db connection

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { Prisma, User } from '@prisma/client';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Post()
+  create(@Body() createUserData: Prisma.UserCreateInput): Promise<User> {
+    return this.appService.create(createUserData);
+  }
+
   @Get()
-  getData() {
-    return this.appService.getData();
+  findAll(): Promise<User[]> {
+    return this.appService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number): Promise<User | null> {
+    return this.appService.findOne(id);
   }
 }
--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { DatabaseService } from '../database/database.service';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Ahoooj Viktorkaaa' };
+  constructor(private prisma: DatabaseService) {}
+
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prisma.user.create({
+      data,
+    });
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.prisma.user.findMany();
+  }
+
+  async findOne(id: number): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: { id },
+    });
   }
 }
