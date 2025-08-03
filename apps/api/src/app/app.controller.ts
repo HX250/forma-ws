4d2354b87c796d032cb82c162db8594201f30143
Rastislav Pacut <rastislavpacut250@gmail.com>
2025-08-03 16:18:23 +0200

    cleaned the project files

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,30 +1,4 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  ParseIntPipe,
-  Post,
-} from '@nestjs/common';
-import { AppService } from './app.service';
-import { Prisma, User } from '@prisma/client';
+import { Controller } from '@nestjs/common';
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Post()
-  create(@Body() createUserData: Prisma.UserCreateInput): Promise<User> {
-    return this.appService.create(createUserData);
-  }
-
-  @Get()
-  findAll(): Promise<User[]> {
-    return this.appService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: number): Promise<User | null> {
-    return this.appService.findOne(id);
-  }
-}
+export class AppController {}
--- apps/web/src/app/app.ts
+++ apps/web/src/app/app.ts
@@ -1,19 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { AppService } from './app.service';
 
 @Component({
-  imports: [RouterModule, FormsModule],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  name: string = '';
   protected title = 'Forma';
-  constructor(private appService: AppService) {}
-  click() {
-    this.appService.mock(this.name);
-  }
 }
