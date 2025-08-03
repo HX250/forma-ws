1e88cc67a6a15f47d1a35979c70a8bbc95f42fbc
Rastislav Pacut <rastislavpacut250@gmail.com>
2025-08-03 13:52:44 +0200

    fixing cors

--- apps/web/src/app/app.ts
+++ apps/web/src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AppService } from './app.service';
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
+  name: string = '';
   protected title = 'Forma';
+  constructor(private appService: AppService) {}
+  click() {
+    this.appService.mock(this.name);
+  }
 }
