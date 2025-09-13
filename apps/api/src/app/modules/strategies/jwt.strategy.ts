e808c15143ba16f97e194db2208b5f8631d8668c
Rastislav Pacut <rastislavpacut250@gmail.com>
2025-09-13 17:29:37 +0200

    changed pipeline

--- apps/api/src/app/modules/strategies/jwt.strategy.ts
+++ apps/api/src/app/modules/strategies/jwt.strategy.ts
@@ -15,7 +15,11 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET,
+      secretOrKey:
+        process.env.JWT_SECRET ||
+        (() => {
+          throw new Error('JWT_SECRET is not defined in environment variables');
+        })(),
     });
   }
 
