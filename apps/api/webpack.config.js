933ac9763aa724d05a1341b3a59912f5541a8b15
Rastislav Pacut <rastislav.pacut@visma.com>
2025-10-25 01:47:25 +0200

    fixed dependency issue

--- apps/api/webpack.config.js
+++ apps/api/webpack.config.js
@@ -15,7 +15,48 @@ module.exports = {
       optimization: false,
       outputHashing: 'none',
       generatePackageJson: true,
-      externalDependencies: ['cookie-parser'],
+
+      externalDependencies: [
+        'cookie-parser',
+        '@nestjs/microservices',
+        '@nestjs/websockets',
+        '@nestjs/platform-socket.io',
+        '@nestjs/websockets/socket-module',
+        '@nestjs/microservices/microservices-module',
+      ],
     }),
   ],
+
+  externals: {
+    '@nestjs/microservices': 'commonjs @nestjs/microservices',
+    '@nestjs/websockets': 'commonjs @nestjs/websockets',
+    '@nestjs/platform-socket.io': 'commonjs @nestjs/platform-socket.io',
+    '@nestjs/websockets/socket-module':
+      'commonjs @nestjs/websockets/socket-module',
+    '@nestjs/microservices/microservices-module':
+      'commonjs @nestjs/microservices/microservices-module',
+  },
+
+  ignoreWarnings: [
+    {
+      module: /node_modules/,
+      message:
+        /Critical dependency: the request of a dependency is an expression/,
+    },
+    {
+      module: /node_modules[\\/]iterare/,
+      message: /Failed to parse source map/,
+    },
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+        exclude: [/node_modules[\\/]iterare/],
+      },
+    ],
+  },
 };
